79a4a7621f1ea0f57ac57d3aeb245684ea91b6a3
Anton <patrin_toxa@mail.ru>
2024-06-27 21:49:19 +0500

    Add booking router

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
+from app.bookings.router import router as booking_router
+
 
 app = FastAPI()
-
+app.include_router(booking_router)
 
 if __name__ == "__main__":
     uvicorn.run('main:app', reload=True)
