31759c5e81d39ff5f0ddeb4940ae3c75c8af0130
Anton <patrin_toxa@mail.ru>
2024-06-27 22:16:26 +0500

    add new method

--- app/dao/base.py
+++ app/dao/base.py
@@ -7,6 +7,13 @@ class BaseDAO:
     model = None
 
     @classmethod
+    async def find_one_or_none(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = select(cls.model.__table__.columns).filter_by(**filter_by)
+            result = await session.execute(query)
+            return result.mappings().one_or_none()
+
+    @classmethod
     async def find_all(cls, **filter_by):
         async with async_session_maker() as session:
             query = select(cls.model.__table__.columns).filter_by(**filter_by)
