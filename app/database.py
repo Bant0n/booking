7d759c40a9a5df576116dffec8376e21f85d6417
Anton <patrin_toxa@mail.ru>
2024-06-27 21:50:29 +0500

    async_session_marker --> async_session_maker

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ from .config import settings
 
 engine = create_async_engine(settings.DATABASE_URL)
 
-async_session_marker = async_sessionmaker(engine, expire_on_commit=False)
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
 class Base(DeclarativeBase):
