ccb9ca8bab7de5356c7500e40429ab32304d2d36
Anton <patrin_toxa@mail.ru>
2024-06-28 22:02:01 +0500

    Add login_user router

--- app/users/router.py
+++ app/users/router.py
@@ -1,6 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response
 
-from app.users.auth import get_password_hash
+from app.users.auth import (
+    authenticate_user,
+    create_access_token,
+    get_password_hash,
+)
 from app.users.dao import UserDAO
 from app.users.schemas import SUserAuth
 
@@ -9,6 +13,11 @@ router = APIRouter(
     prefix="/users",
     tags=["Auth & Пользователи"],
 )
+
+
+@router.get("/users")
+async def get_users():
+    return await UserDAO.find_all()
 
 
 @router.post("/register")
@@ -24,6 +33,15 @@ async def register_uer(user_data: SUserAuth):
     await UserDAO.add(email=user_data.email, hashed_password=hashed_password)
 
 
-@router.get('/users')
-async def get_users():
-    return await UserDAO.find_all()
+@router.post("/login")
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(user_data.email, user_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=401, detail="Неправильные данные для входа"
+        )
+
+    access_token = create_access_token({"sub": str(user.id)})
+    response.set_cookie("booking_access_token", access_token, httponly=True)
+    return access_token
