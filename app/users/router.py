fbc4dfb3c0cb67cacc20441777a0ea1735022854
Anton <patrin_toxa@mail.ru>
2024-06-28 21:36:11 +0500

    Initial users router, add 'register', 'users' routers

---
+++ app/users/router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+
+from app.users.auth import get_password_hash
+from app.users.dao import UserDAO
+from app.users.shemas import SUserRegister
+
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Auth & Пользователи"],
+)
+
+
+@router.post("/register")
+async def register_uer(user_data: SUserRegister):
+    existing_user = await UserDAO.find_one_or_none(email=user_data.email)
+
+    if existing_user:
+        raise HTTPException(
+            status_code=401, detail="Пользователь уже существует"
+        )
+
+    hashed_password = get_password_hash(user_data.password)
+    await UserDAO.add(email=user_data.email, hashed_password=hashed_password)
+
+
+@router.get('/users')
+async def get_users():
+    return await UserDAO.find_all()
