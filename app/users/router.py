39d7445d69feb9f68fef22d1f6c93f71e22b4c73
Anton <patrin_toxa@mail.ru>
2024-07-01 16:44:27 +0500

    Add 'me' router

--- app/users/router.py
+++ app/users/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Response, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 
 from app.users.auth import (
     authenticate_user,
@@ -6,6 +6,8 @@ from app.users.auth import (
     get_password_hash,
 )
 from app.users.dao import UserDAO
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 from app.users.schemas import SUserAuth
 
 
@@ -48,6 +50,11 @@ async def login_user(response: Response, user_data: SUserAuth):
     return access_token
 
 
-@router.post('/logout')
+@router.post("/logout")
 async def logout_user(response: Response):
     response.delete_cookie("booking_access_token")
+
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
