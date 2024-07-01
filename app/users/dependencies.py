ae67e474b203baa05b15775b2d42804f0300a5cb
Anton <patrin_toxa@mail.ru>
2024-07-01 17:15:34 +0500

    Add custom exceptions

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -1,18 +1,22 @@
 from datetime import datetime
-from fastapi import Depends, HTTPException, Request, status
-from jose import jwt, JWTError
+
+from fastapi import Depends, Request
+from jose import JWTError, jwt
 
 from app.config import settings
+from app.exceptions import (
+    IncorrectTokenFormatException,
+    TokenAbsentException,
+    TokenExpiredException,
+    UserIsNotPresentException,
+)
 from app.users.dao import UserDAO
 
 
 def get_token(request: Request):
     token = request.cookies.get("booking_access_token")
     if not token:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Необходима авторизация",
-        )
+        raise TokenAbsentException
     return token
 
 
@@ -22,26 +26,18 @@ async def get_current_user(token: str = Depends(get_token)):
             token, key=settings.KEY, algorithms=settings.ALGORITHM
         )
     except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-        )
+        raise IncorrectTokenFormatException
 
     expire: str = payload.get("exp")
     if (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-        )
+        raise TokenExpiredException
 
     user_id: str = payload.get("sub")
     if not user_id:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-        )
+        raise UserIsNotPresentException
 
     user = await UserDAO.find_one_or_none(id=int(user_id))
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-        )
+        raise UserIsNotPresentException
 
     return user
