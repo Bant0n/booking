42acc4d804143fc484a360ff9a2713f0b93cc314
Anton <patrin_toxa@mail.ru>
2024-07-08 21:21:28 +0500

    Сhanged the response scheme

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -3,7 +3,7 @@ from datetime import date
 from fastapi import APIRouter, Depends
 
 from app.bookings.dao import BookingDAO
-from app.bookings.schemas import SBooking
+from app.bookings.schemas import SBookingRoom
 from app.exceptions import RoomCannotBeBookedException
 from app.users.dependencies import get_current_user
 from app.users.models import Users
@@ -14,8 +14,8 @@ router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 @router.get("")
 async def get_bookings(
     user: Users = Depends(get_current_user),
-) -> list[SBooking]:
-    return await BookingDAO.find_all(id=user.id)
+) -> list[SBookingRoom]:
+    return await BookingDAO.find_by_user(user_id=user.id)
 
 
 @router.post("")
