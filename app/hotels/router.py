196a0551641985ec6f683ddd6f7735993ddace9f
Anton <patrin_toxa@mail.ru>
2024-07-09 16:19:01 +0500

    Add schemas and router

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,7 +1,9 @@
 from datetime import date
+
 from fastapi import APIRouter
 
 from app.hotels.dao import HotelDAO
+from app.hotels.schemas import SHotel, SHotelInfo
 
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
 
@@ -16,5 +18,12 @@ async def get_hotel(
     location: str,
     date_from: date,
     date_to: date,
-):
+) -> list[SHotelInfo]:
     return await HotelDAO.get_by_location(location, date_from, date_to)
+
+
+@router.get("/id/{hotel_id}", include_in_schema=True)
+async def get_hotel_by_id(
+    hotel_id: int,
+) -> SHotel | None:
+    return await HotelDAO.find_one_or_none(id=hotel_id)
