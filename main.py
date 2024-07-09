3f2ea5ad13f4a8a873a6cfd1900ceffdf81c9890
Anton <patrin_toxa@mail.ru>
2024-07-09 16:06:00 +0500

    Add router

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
 import uvicorn
-from app.bookings.router import router as bookings_router
-from app.users.router import router as users_router
+from fastapi import FastAPI
 
+from app.bookings.router import router as bookings_router
+from app.hotels.rooms.router import router as hotels_router
+from app.users.router import router as users_router
 
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(bookings_router)
+app.include_router(hotels_router)
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run("main:app", reload=True)
