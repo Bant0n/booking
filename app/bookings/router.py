6e11cac8be8b1b5efdd400a61592592d0a8c01d2
Anton <patrin_toxa@mail.ru>
2024-07-09 16:03:57 +0500

    Add SNewBooking and delete router

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,9 +1,7 @@
-from datetime import date
-
 from fastapi import APIRouter, Depends
 
 from app.bookings.dao import BookingDAO
-from app.bookings.schemas import SBookingRoom
+from app.bookings.schemas import SBookingRoom, SNewBooking
 from app.exceptions import RoomCannotBeBookedException
 from app.users.dependencies import get_current_user
 from app.users.models import Users
@@ -20,14 +18,23 @@ async def get_bookings(
 
 @router.post("")
 async def add_booking(
-    room_id: int,
-    date_from: date,
-    date_to: date,
+    booking: SNewBooking,
     user: Users = Depends(get_current_user),
 ):
     booking = await BookingDAO.add(
-        user_id=user.id, room_id=room_id, date_from=date_from, date_to=date_to
+        user_id=user.id,
+        room_id=booking.room_id,
+        date_from=booking.date_from,
+        date_to=booking.date_to,
     )
 
     if not booking:
         raise RoomCannotBeBookedException
+
+
+@router.delete("/{booking_id}")
+async def delete_booking(
+    booking_id: int,
+    user: Users = Depends(get_current_user),
+):
+    await BookingDAO.delete(booking_id=booking_id, user_id=user.id)
