c24c61ea46e676a60e8f6227b452250d4e79a23f
Anton <patrin_toxa@mail.ru>
2024-07-12 14:52:24 +0500

    Use ConfigDict

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 from typing import Literal
 
 from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
 
 
 class Settings(BaseSettings):
@@ -24,8 +25,10 @@ class Settings(BaseSettings):
     def TEST_DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"  # noqa
 
-    class Config:
-        env_file = ".env"
+    # class Config():
+    #     env_file = ".env"
+
+    model_config = ConfigDict(env_file=".env")
 
     @property
     def DATABASE_URL(self):
