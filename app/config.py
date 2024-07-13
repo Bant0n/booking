c1a6f2270f70484902b69be89042bcdbab3d9661
Anton <patrin_toxa@mail.ru>
2024-07-13 21:46:59 +0500

    Add redis host and port

--- app/config.py
+++ app/config.py
@@ -21,6 +21,9 @@ class Settings(BaseSettings):
     TEST_DB_PASS: str
     TEST_DB_NAME: str
 
+    REDIS_HOST: str
+    REDIS_PORT: int
+
     @property
     def TEST_DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"  # noqa
